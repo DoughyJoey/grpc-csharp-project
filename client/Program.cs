8e034ab068b6acd27806dbae65eea22df57f5b45
DoughyJoey <x15037355@student.ncirl.ie>
2020-03-31 12:40:03 +0100

    created sleep average & max heart rate services to demonstrate interoperability with java project

---
+++ client/Program.cs
@@ -0,0 +1,69 @@
+﻿using Grpc.Core;
+using SmartWatch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        const string target = "127.0.0.1:50053";
+
+        static async Task Main(string[] args)
+        {
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+
+            await channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("The client connected successfully");
+            });
+
+            var client = new WatchService.WatchServiceClient(channel);
+            var stream = client.SleepAverage();
+
+            foreach (int number in Enumerable.Range(6, 8))
+            {
+                var request = new SleepAverageRequest() { Number = number };
+
+                await stream.RequestStream.WriteAsync(request);
+            }
+
+            await stream.RequestStream.CompleteAsync();
+
+            var response = await stream.ResponseAsync;
+
+            Console.WriteLine("Your average sleep this week is: " + response.Average + " hrs");
+
+            //channel.ShutdownAsync().Wait();
+            //Console.ReadKey();
+
+
+            // MAX HEART RATE
+            var client2 = new WatchService.WatchServiceClient(channel);
+            var stream2 = client.MaxHeartRate();
+
+            var responseReaderTask = Task.Run(async () =>
+            {
+                while (await stream2.ResponseStream.MoveNext())
+                    Console.WriteLine("Your current heart rate is: " + stream2.ResponseStream.Current.Maximum);
+            });
+
+            int[] numbers = { 66, 77, 73, 87, 91, 101 };
+
+            foreach (var number in numbers)
+            {
+                await stream2.RequestStream.WriteAsync(new MaxHeartRateRequest() { Number = number });
+            }
+
+            await stream2.RequestStream.CompleteAsync();
+            await responseReaderTask;
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ server/WatchServiceImpl.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Core;
+using SmartWatch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SmartWatch.WatchService;
+
+namespace server
+{
+    public class WatchServiceImpl : WatchServiceBase
+    {
+
+        public override async Task<SleepAverageResponse> SleepAverage(IAsyncStreamReader<SleepAverageRequest> requestStream, ServerCallContext context)
+        {
+            int total = 0;
+            double average = 0.0;
+
+            while (await requestStream.MoveNext())
+            {
+                average += requestStream.Current.Number;
+                total++;
+            }
+
+            return new SleepAverageResponse() { Average = average / total };
+        }
+
+        public override async Task MaxHeartRate(IAsyncStreamReader<MaxHeartRateRequest> requestStream, IServerStreamWriter<MaxHeartRateResponse> responseStream, ServerCallContext context)
+        {
+            int? max = null;
+
+            while (await requestStream.MoveNext())
+            {
+                if (max == null || max < requestStream.Current.Number)
+                {
+                    max = requestStream.Current.Number;
+                    await responseStream.WriteAsync(new MaxHeartRateResponse() { Maximum = max.Value });
+                }
+            }
+        }
+    }
+}
